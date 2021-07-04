3ae5dbc6a4db303f76e5d9f4533de44a84401fda
Dvin <dvyu@yandex.ru>
2021-07-04 14:18:40 +0500

    add credentials file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,16 @@
 # exit when any command fails
 set -e
 
+# create credentials file
+(
+cat << EOF
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
+) > /credentials
+
+# run checks
 cd $WORKDIR
 terraform init
 terraform plan
